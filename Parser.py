2c7b84761c786774f22cefdad63105fb7a325782
Eugene <enderbro.13@mail.ru>
2020-04-25 12:16:46 +0300

    Some fixes

--- Parser.py
+++ Parser.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-# url = 'http://commencal-russia.ru/product-category/bikes/2020/enduro-2020/'
-# r = requests.get(url)
-# with open('test.html', 'wb') as out:
-#     out.write(r.text.encode('cp1251'))
+url = 'http://commencal-russia.ru/product-category/bikes/2020/enduro-2020/'
+r = requests.get(url)
+with open('test.html', 'wb') as out:
+    out.write(r.text.encode('cp1251'))
 
 with open('test.html') as input_file:
     text = input_file.read() 
